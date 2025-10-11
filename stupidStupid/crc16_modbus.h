b9ebc7d4829c53ad10932a13a330885453bd561a
96liuzhixin <zhixin.liu@dfrobot.com>
2025-10-11 17:18:01 +0800

    v0.0.1 first

---
+++ stupidStupid/crc16_modbus.h
@@ -0,0 +1,10 @@
+#ifndef __CRC16_MODBUS_H
+#define __CRC16_MODBUS_H
+
+#include "Arduino.h"
+#define usMBCRC16   CalcCRC16_Modbus
+
+uint16_t CalcCRC16_Modbus( uint8_t *Frame, uint16_t Len );
+
+
+#endif /*_CRC16_MODBUS_H */
